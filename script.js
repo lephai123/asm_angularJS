fb326cd9efdfe970029193a31ca639653b9c8ebe
lephai123 <phailqph20664@fpt.edu.vn>
2023-04-06 10:58:32 +0700

    the first

---
+++ script.js
@@ -0,0 +1,34 @@
+// Lấy tất cả các link trong menu
+const links = document.querySelectorAll('nav ul li a');
+
+// Duyệt qua các link và thêm sự kiện click vào mỗi link
+links.forEach(link => {
+  link.addEventListener('click', function() {
+    // Loại bỏ class active từ tất cả các link
+    links.forEach(link => {
+      link.classList.remove('active');
+    });
+
+    // Thêm class active vào link được click
+    this.classList.add('active');
+  });
+});
+
+// Thêm class active vào link đầu tiên khi trang được tải
+links[0].classList.add('active');
+
+function showpay(){
+  var selectValue = document.getElementById("floatingSelect").value;
+  if(selectValue == "1"){
+    document.getElementById("pay_visa").style.display = "block";
+  }
+  else {
+    document.getElementById("pay_visa").style.display = "none";
+  }
+  if(selectValue == "2"){
+    document.getElementById("eBanking").style.display = "block";
+  }
+  else {
+    document.getElementById("eBanking").style.display = "none";
+  }
+}
